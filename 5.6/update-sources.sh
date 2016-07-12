d03da7072a4a6745e6cc75c6337bf82ac5400dfc
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-07-12 11:06:10 +0300

    Added php 5.6 - extension of the standard php 5.6 image

---
+++ 5.6/update-sources.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")""
+
+rm -rf $THIS_DIR/apache
+
+cd $THIS_DIR/../../php-official/5.6
+
+git pull origin master
+
+cp -R ./apache  $THIS_DIR
+
+cd $THIS_DIR
+
+echo "Adding ftp and zip modules..."
+sed -i "s/--with-openssl/--with-openssl --enable-ftp --enable-zip/g" ./apache/Dockerfile
