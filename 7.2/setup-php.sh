720fcd6214691461d83040f1468fcba5415808ec
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2018-05-04 10:56:33 +0300

    php 7.2 added with custom environment variables

---
+++ 7.2/setup-php.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INI_DIR="/usr/local/etc/php/conf.d"
+
+
+DNH_INI_FILE="$INI_DIR/dnh-custom.ini"
+
+rm -f $DNH_INI_FILE
+
+echo "upload_max_filesize=$PHP_UPLOAD_MAX_FILESIZE" >> $DNH_INI_FILE
+echo "post_max_size=$PHP_POST_MAX_FILESIZE" >> $DNH_INI_FILE
+echo "memory_limit=$PHP_MEMORY_LIMIT" >> $DNH_INI_FILE
+echo "max_execution_time=$PHP_MAX_EXECUTION_TIME" >> $DNH_INI_FILE
+echo "session.cookie_secure=$PHP_SESSION_COOKIE_SECURE" >> $DNH_INI_FILE
+
+
+OPCACHE_INI_FILE="$INI_DIR/opcache-custom.ini"
+
+rm -f $OPCACHE_INI_FILE
+
+if [ "$SWPHP_OPCACHE_ENABLE" == "1" ]; then
+  echo "zend_extension=opcache.so" >> $OPCACHE_INI_FILE
+  echo "opcache.enable=1" >> $OPCACHE_INI_FILE
+  echo "opcache.memory_consumption=$PHP_OPCACHE_MEMORY_CONSUMPTION" >> $OPCACHE_INI_FILE
+  echo "opcache.interned_strings_buffer=$PHP_OPCACHE_INTERNED_STRINGS_BUFFER" >> $OPCACHE_INI_FILE
+  echo "opcache.max_accelerated_files=$PHP_OPCACHE_ACCELERATED_FILES" >> $OPCACHE_INI_FILE
+  echo "opcache.revalidate_freq=$PHP_OPCACHE_REVALIDATE_FREQ" >> $OPCACHE_INI_FILE
+  echo "opcache.fast_shutdown=$PHP_OPCACHE_FAST_SHUTDOWN" >> $OPCACHE_INI_FILE
+fi
